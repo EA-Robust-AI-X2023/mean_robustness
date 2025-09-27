b2fbe174a2eab3eaf23270195b73df73f406ffdc
slivering <47292273+slivering@users.noreply.github.com>
2025-09-28 00:11:44 +0200

    Allow Jupyter notebooks in subdirectories

---
+++ mero/dirs.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent.absolute() # The project root directory
+
+CHECKPOINTS_DIR = ROOT_DIR / "checkpoints" # Model checkpoints directory
+DATA_DIR = ROOT_DIR / "data"               # Datasets directory
+FIGURES_DIR = ROOT_DIR / "reports/figures" # Figures directory
