bc85882a8af396f6c0f4c34a55ff7e91ef0cdfc9
mathias <mathias.ollu@polytechnique.edu>
2025-11-03 01:34:25 +0600

    files to launch the tests. Import of the variance_and_plot branch required

---
+++ scripts/first_tests/plots_test.py
@@ -0,0 +1,9 @@
+from byzfl.benchmark.evaluate_results import test_accuracy_curve
+
+path_training_results = "results"
+path_to_plot = "plot"
+
+test_accuracy_curve(
+    path_training_results,
+    path_to_plot
+)
